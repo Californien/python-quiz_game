85cd1e94037c89da03a417076535371aaebc94e3
Marlin L. <96547308+Californien@users.noreply.github.com>
2025-02-05 14:00:10 +0100

    Create gameLogic.py

---
+++ gameLogic.py
@@ -0,0 +1,6 @@
+def runQuiz():
+    print('Quiz start...')
+    # score = 0
+    # max_score = len(questions)
+    # for q in questions:
+    #     print(f'[{q['q_id']}] {q['content']['question']}')
