2f1ad978f622e9a9c3bd6b88f3c186c42eafb318
Marlin L. <96547308+Californien@users.noreply.github.com>
2025-02-13 13:06:41 +0100

    Update gameLogic.py

--- gameLogic.py
+++ gameLogic.py
@@ -1,14 +1,18 @@
 import time
 import keyboard
+import random
+from colorama import Fore, Style
 
-def activateBuzzer():
-    print('buzzer')
-    while True:
-        if keyboard.is_pressed('b'):
-            return 'b' 
+def activateBuzzer(buttons, function, *args):
+    print(f'\n{Fore.LIGHTWHITE_EX + Style.BRIGHT + '>>>>    '}{Fore.LIGHTCYAN_EX + Style.BRIGHT + 'BUZZER'}{Fore.LIGHTWHITE_EX + Style.BRIGHT + '    <<<<'}')
+    i = 1
+    for el in buttons:
+        print(f'{Fore.LIGHTBLACK_EX + Style.NORMAL + f'Spieler {i}:'} {Fore.LIGHTMAGENTA_EX + Style.BRIGHT + f'[{el.upper()}] <'}')
+        i += 1
 
-def runQuiz():
+def runQuiz(arg1):
     print('Quiz start...')
+    print(f'{arg1}')
     # score = 0
     # max_score = len(questions)
     # for q in questions:
